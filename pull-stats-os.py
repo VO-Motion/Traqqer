1cff25185c146805babb2b56c065a8a2d8975267
VO-Motion <103268456+VO-Motion@users.noreply.github.com>
2022-04-11 16:47:20 +0200

    Update pull-stats-os.py

--- pull-stats-os.py
+++ pull-stats-os.py
@@ -1,15 +1,28 @@
-from opensea import OpenseaAPI # Use pip to install OpenSea API: cmd -> pip3 install opensea
+from opensea import OpenseaAPI
+import pandas as pd
+import time
 
 # OPENSEA API
 
-api = OpenseaAPI(apikey='{Private API Key}')
+api = OpenseaAPI(apikey='prive-api-key')
+collection_database = pd.DataFrame
+collection_data = {}
 
-def fetch_raw_collection(collection_slug):
+def fetch_collection_data(collection_slug):
     return api.collection(collection_slug)
 
-def fetch_collection(collection_slug):
-    response = fetch_raw_collection(collection_slug)
-    data = response['collection']['stats']
-    print(data)
+def fetch_collection_stats(collection_slug):
+    response = fetch_collection_data(collection_slug)
+    stats = response['collection']['stats']
+    name = response['collection']['name']
+    return name, stats
 
-fetch_collection('pudgypenguins') # Example to pull statistics for Pudgy Penguins
+def poll():
+    collection_list = open("/traqqer/back-end/collection_list.txt").read().splitlines()
+
+    for x in collection_list:
+        temp_data = fetch_collection_stats(x)
+        collection_data[temp_data[0]] = temp_data[1]
+        
+    collection_database = pd.DataFrame(collection_data).transpose()
+    print(collection_database) # print if needed
